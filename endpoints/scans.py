e0f21d748ad77e8f778aa7445ea7e8964f954077
Neakio <l.olivie@hotmail.com>
2025-08-05 11:03:30 +0200

    Revert "Update scans.py"
    This reverts commit 1b4ebcfd1296ddd68e624b47c58f1641ff1a0c0b.

--- endpoints/scans.py
+++ endpoints/scans.py
@@ -1,155 +1,138 @@
 # ------------------------------ PACKAGES ------------------------------
-# General packages
+# Standard imports
 import asyncio
-import subprocess
-import pty
-from requests import exceptions, post
-from datetime import datetime
-from os import getenv, path
-# Internal packages
+from dotenv import load_dotenv
+
+# Third-party libraries
+from fastapi import (
+    Query,
+    File,
+    UploadFile,
+    Depends,
+    Request,
+    APIRouter,
+)
+from fastapi.responses import JSONResponse
+
+# Local imports
 import functions.utils as utils
+import functions.scan as scan
+import endpoints.security as security
+
+# Database
+import postgres.models as models
 
 
+################################## [ INIT ] ##################################
+load_dotenv()
+profiles, formats, workflows = utils.load_json()
+ValidprofilesEnum = utils.create_enum("ValidprofilesEnum", profiles)
+ValidformatsEnum = utils.create_enum("ValidformatsEnum", formats)
+ValidworkflowsEnum = utils.create_enum("ValidworkflowsEnum", workflows)
 
-# ------------------------------ PROCESSING ------------------------------
-async def processing(q, file, output, client_ip=""):
-    """Prepare API request for the scan and call it
+router = APIRouter(prefix="/scans", tags=["scans"])
 
-    Args:
-        q (str): Profile/Workflow chosen
-        domain (str): Input filename
-        output (str, optional): Output type. Default empty.
-        client_ip (str, optional): Requester IP address used to send result. Default empty.
-    """
-    utils.api_log(
-        f"API call received. Start processing for {file}. The client_ip is {client_ip}"
+scan_queue = asyncio.Queue()
+
+################################## [ FUNCTION ] ##################################
+
+
+async def process_queue():
+    """Continuously monitor the queue for new scan jobs."""
+    while True:
+        job = await scan_queue.get()
+        await handle_scan(job)
+        scan_queue.task_done()
+
+
+async def handle_scan(job):
+    """Process each scan job one at a time."""
+    code = await scan.processing(
+        job["q"], job["domain"], job["output"], job["uuid"], job["client_ip"]
     )
-    domain, ext = path.splitext(file)
-    current_datetime = datetime.now().strftime("%Y-%m-%d")
-    outfile = f"{current_datetime}_{q.value}{domain}.{format}"
-    utils.api_log(f"Output filename: {outfile}")
-
-    code = await scan(input=file, output=outfile, profile=(q.value), format=output)
-
-    status = "completed" if code == 0 else "error"
-    return status
-
-
-# ------------------------- Main scan function -------------------------
-async def scan(input, output, profile=None, format):
-    """Run axiom-scan based on arguments provided
-
-    Args:
-        input (str): Input filename
-        output (str): Output filename
-        profile (str, optional): Single scan case. Defaults to None.
-        format (str, optional): Output type. Default empty.
-
-    Returns:
-        code: return error/success code
-    """
-    tool = None
-    outype = None
-    count = 0
-    utils.axiom_log("-----------------------")
-    match profile:
-        case "ip_list":
-            tool = "dnsx -re"
-        case "dns_list":
-            tool = "amass"
-            return
-        case "web_list":
-            tool = "gau"
-        case "waf_check":
-            tool = "wafw00f"
-            utils.add_https_to_each_line(input)
-        case "ssl_check":
-            tool = "testssl"
-            utils.add_https_to_each_line(input)
-        case "http_check":
-            tool = "httpx -fr -sc -location -title -method"
-        case "dns_check":
-            tool = "whois"
-            return
-        case "web_scan":
-            tool = "aquatone"
-        case "port_scan":
-            tool = "nmap -sV -sC"
-        case _:
-            utils.axiom_log(f"Invalid profile: {profile}, discarding scan")
-            utils.axiom_log("-----------------------")
-            return 1
-    utils.axiom_log(f"Tool used: {tool}")
-    match format:
-        case "txt":
-            outype = "-o"
-        case "json":
-            outype = "-oJ"
-        case "html":
-            outype = "-oH"
-        case _:
-            utils.axiom_log(f"Invalid format: {format}, discarding scan")
-            utils.axiom_log("-----------------------")
-            return 1
-    utils.axiom_log(f"Output format: {outype}")
-    count = 0
-    # Determine the file type and count the number of lines, entries, or rows
-    if ".json" in input:
-        count = utils.count_entries_in_json(f"/var/tmp/scan_input/{input}")
-    elif ".csv" in input:
-        count = utils.count_rows_in_csv(f"/var/tmp/scan_input/{input}")
-    elif ".txt" in input:
-        count = utils.count_lines_in_txt(f"/var/tmp/scan_input/{input}")
+    if code == "completed":
+        utils.api_log(f"Scan for {job['domain']} completed successfully.")
     else:
-        utils.axiom_log(f"Incorrect input filename : {input}")
+        utils.api_log(f"Scan for {job['domain']} failed.")
 
-    with open(f"/var/tmp/scan_input/{input}", "r") as file:
-        lines_list = [line.strip() for line in file.readlines()]
-    await utils.instances_needed(count)  # Start needed instances
 
-    starttime = datetime.now().strftime("%H:%M:%S")
-    await axiom(tool, outype, input, f"/var/tmp/scan_output/{output}", profile)
-    endtime = datetime.now().strftime("%H:%M:%S")
-    utils.save_to_bucket(output)
-    length = f"{starttime} - {endtime}"
-    utils.cert_json(lines_list, tool, length)
-    utils.stop_instances()
-    subprocess.run(
-        [f"rm /var/tmp/scan_input/{input}"],
-        shell=True,
-        check=False
+################################### [ API ] ##################################
+# ------------------------------ Scan Execution ------------------------------
+
+
+# API endpoint for unique scan
+@router.get("/")
+async def single_scan(
+    request: Request,
+    current_user: models.User = Depends(security.get_current_user),
+    q: ValidprofilesEnum = Query(..., description="Must be one of the valid values."),
+    domain: str = Query(..., min_length=1, description="Cannot be empty"),
+    output: ValidformatsEnum = Query(
+        None, description="Optional format. Must be one of the valid values."
+    ),
+    uuid: str = Query(None, min_length=1, description="Optional to notify end of scan"),
+):
+    if output is None:
+        output = "txt"
+    else:
+        output = output.value
+    utils.api_log(
+        f"Single scan requested by {current_user.email} (IP : {request.client.host}). Domain is {domain} and case is {q.value}"
     )
-    utils.axiom_log("-----------------------")
-    return 0
+    filename = f"{domain}.txt"
+    with open(
+        f"/var/tmp/scan_input/{filename}", "w", encoding="utf-8"
+    ) as f:  # Save single input as file in input folder
+        f.write(f"{domain}\n")
+    utils.api_log(f"Temporary file saved as /var/tmp/scan_input/{filename}")
 
-async def axiom(module, outype, input, output, profile):
-    axiom_path = getenv("AXIOM_PATH")
-    home = getenv("HOME")
-    env = {
-        "TERM": "xterm",
-        "HOME": str(home),
-        "PATH": "/home/ubuntu/go/bin:/usr/local/go/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.local/bin:/home/ubuntu/.axiom/interact"
+    request_data = {
+        "domain": filename,
+        "q": q,
+        "output": output,
+        "uuid": uuid,
+        "client_ip": request.client.host,
     }
-    command = f"{axiom_path}axiom-scan /var/tmp/scan_input/{input} -m {module} {outype} {output}"
-    utils.axiom_log(f"Start of {profile} for /var/tmp/scan_input/{input}")
-    utils.axiom_log(f"{command}")
-    master_fd, slave_fd = pty.openpty()
-    process = subprocess.Popen(
-        command, shell=True, stdin=slave_fd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, env=env, close_fds=True
+
+    # Append the job to the in-memory queue
+    await scan_queue.put(request_data)
+    utils.api_log("Job sent to queue")
+
+    # Return immediately to the requester
+    return JSONResponse({"message": "Job sent to queue"})
+
+
+# API endpoint for file scan
+@router.post("/")
+async def file_scan(
+    request: Request,
+    current_user: models.User = Depends(security.get_current_user),
+    q: ValidprofilesEnum = Query(..., description="Must be one of the valid values."),
+    domain: UploadFile = File(...),
+    output: ValidformatsEnum = Query(
+        None, description="Optional format. Must be one of the valid values."
+    ),
+    uuid: str = Query(None, min_length=1, description="Optional to notify end of scan"),
+):
+    if output is None:
+        output = "txt"
+    else:
+        output = output.value
+    contents = await domain.read()  # Wait & Read uploaded file
+    utils.api_log(
+        f"File scan requested by {current_user.email} (IP : {request.client.host}). File is here /var/tmp/scan_input/{domain.filename} and case is {q.value}"
     )
-    stdout, stderr = await asyncio.get_event_loop().run_in_executor(None, process.communicate)
-    if process.returncode != 0 or "exiting" in stdout:
-        utils.axiom_log("Command failed with error: ")
-        utils.axiom_log(stderr)
-        utils.axiom_log("Command output: ")
-        utils.axiom_log(stdout)
-        utils.axiom_log(
-            f"End of {profile} using {module}, see error at: /var/log/dnsscan/axiom.log"
-        )
-        return 1
-    utils.axiom_log("Command output:")
-    utils.axiom_log(stdout)
-    utils.axiom_log(
-        f"End of {profile} using {module}, succesfull result: {output}\n"
-    )
-    return 0
+    with open(f"/var/tmp/scan_input/{domain.filename}", "wb") as f:
+        f.write(contents)
+    request_data = {
+        'domain': domain.filename,
+        'q': q,
+        'output': output,
+        'uuid': uuid,
+        'client_ip': request.client.host
+    }
+    await scan_queue.put(request_data)
+    utils.api_log("Job sent to queue")
+
+    # Return immediately to the requester
+    return JSONResponse({"message": "Job sent to queue"})
